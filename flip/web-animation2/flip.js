6e460866cd2961427a4399de4625680fd7f6a0f5
Rumen Neshev <rumen@KOMERO.NET>
2019-12-10 11:20:32 +0200

    asd

--- flip/web-animation2/flip.js
+++ flip/web-animation2/flip.js
@@ -3,136 +3,128 @@ const optsDef = {
   easing: "ease-out"
 };
 
-/**
- * @param {HTMLElement}
- * @param {Object} opts
- */
-export default function flip(el, opts = {}) {
-  opts = { ...optsDef, ...opts };
+// /**
+//  * @param {HTMLElement}
+//  * @param {Object} opts
+//  */
+// export default function flip(el, opts = {}) {
+//   opts = { ...optsDef, ...opts };
 
-  // Get the first position.
-  const first = el.getBoundingClientRect();
+//   // Get the first position.
+//   const first = el.getBoundingClientRect();
 
-  // Move it to the end.
-  if (typeof opts.triggerAction === "string")
-    el.classList.add(opts.triggerAction);
-  else if (typeof opts.triggerAction === "function") opts.triggerAction();
+//   // Move it to the end.
+//   if (typeof opts.triggerAction === "string")
+//     el.classList.add(opts.triggerAction);
+//   else if (typeof opts.triggerAction === "function") opts.triggerAction();
 
-  // Get the last position.
-  const last = el.getBoundingClientRect();
+//   // Get the last position.
+//   const last = el.getBoundingClientRect();
 
-  // Invert.
-  const deltaX = first.left - last.left;
-  const deltaY = first.top - last.top;
-  const deltaW = first.width / last.width;
-  const deltaH = first.height / last.height;
+//   // Invert.
+//   const deltaX = first.left - last.left;
+//   const deltaY = first.top - last.top;
+//   const deltaW = first.width / last.width;
+//   const deltaH = first.height / last.height;
 
-  // Go from the inverted position to last.
-  const player = el.animate(
-    [
-      {
-        transformOrigin: "top left",
-        transform: `translate(${deltaX}px, ${deltaY}px) scale(${deltaW}, ${deltaH})`
-      },
-      {
-        transformOrigin: "top left",
-        transform: "none"
-      }
-    ],
-    {
-      duration: opts.duration,
-      easing: opts.easing
-    }
-  );
+//   // Go from the inverted position to last.
+//   const player = el.animate(
+//     [
+//       {
+//         transformOrigin: "top left",
+//         transform: `translate(${deltaX}px, ${deltaY}px) scale(${deltaW}, ${deltaH})`
+//       },
+//       {
+//         transformOrigin: "top left",
+//         transform: "none"
+//       }
+//     ],
+//     {
+//       duration: opts.duration,
+//       easing: opts.easing
+//     }
+//   );
 
-  // Do any tidy up at the end
-  // of the animation.
-  if (opts.finishCallback)
-    player.addEventListener("finish", opts.finishCallback);
-}
-
+//   // Do any tidy up at the end
+//   // of the animation.
+//   if (opts.finishCallback)
+//     player.addEventListener("finish", opts.finishCallback);
+// }
 
 /**
  * Animate layout change using the FLIP principle
- * @param {Array|Element} elements - Elements to animate
- * @param {Function} doChange - Function which applies the layout change
- * @param {Function} callback - Function which is called when the animation ends
- * @param {Object} options - Optional options. See `opts` for properties
+ * @param {HTMLElement[]} elements - Elements to animate
+ * @param {Function} triggerAction - Function which applies the layout change
+ * @param {Function} [finishCallback] - Function which is called when the animation ends
+ * @param {Object} [options] - Optional options. See `opts` for properties
  */
-function FLIP(elements, doChange, callback, options) {
-    // merge options
-    var opts = {
-      time: 0.4, // time it takes to animate
-      animatingClass: "flip-animating", // class to apply to elements being animated
-      scalingClass: "flip-scaling" // class to apply to elements being scaled
+export default function flip(
+  elements,
+  triggerAction,
+  finishCallback,
+  options = {}
+) {
+  // normalize elements to array
+  /**
+   * @type {HTMLElement[]}
+   */
+  elements = elements.length !== undefined ? elements : [elements];
+
+  // nothing to animate
+  if (!elements.length) return;
+
+  // merge options
+  options = { ...optsDef, ...options };
+
+  // get initial positions
+  const startRecs = elements.map(el => el.getBoundingClientRect());
+
+  // change layout
+  triggerAction();
+
+  // get end positions
+  const endRecs = elements.map(el => el.getBoundingClientRect());
+
+  // invert - move back to old positions
+  const deltas = endRecs.map((endRec, index) => {
+    const startRec = startRecs[index];
+    return {
+      x: startRec.left - endRec.left,
+      y: startRec.top - endRec.top,
+      scaleWidth: startRec.width / endRec.width,
+      scaleHeight: startRec.height / endRec.height
     };
-    for (var k in options) {
-      opts[k] = options[k];
-    }
-  
-    // normalize elements to array
-    elements = elements.length !== undefined ? elements : [elements];
-  
-    // get initial positions
-    var pres = [];
-    for (var i = 0, j = elements.length; i < j; ++i) {
-      pres[i] = elements[i].getBoundingClientRect();
-    }
-  
-    // change layout
-    doChange();
-  
-    // move back to old positions
-    for (i = 0; i < j; ++i) {
-      var elm = elements[i],
-        pre = pres[i],
-        post = elm.getBoundingClientRect();
-  
-      // calculating from middle of object
-      // this works with flexbox (calculating from corner does not)
-      var delta = {
-        left: pre.left + pre.width / 2 - (post.left + post.width / 2),
-        top: pre.top + pre.height / 2 - (post.top + post.height / 2),
-        scaleWidth: pre.width / post.width,
-        scaleHeight: pre.height / post.height
-      };
-  
-      elm.style.transform =
-        "translate(" +
-        delta.left.toFixed(2) +
-        "px," +
-        delta.top.toFixed(2) +
-        "px) " +
-        "scale(" +
-        delta.scaleWidth.toFixed(2) +
-        "," +
-        delta.scaleHeight.toFixed(2) +
-        ")";
-  
-      elm.classList.add(opts.animatingClass);
-      if (
-        Math.round(delta.scaleWidth * 100) !== 100 ||
-        Math.round(delta.scaleHeight * 100) !== 100
-      ) {
-        elm.classList.add(opts.scalingClass);
-      }
-    }
-  
-    // wait for styles to apply, then animate to new position
-    requestAnimationFrame(function() {
-      for (i = 0; i < j; ++i) {
-        elements[i].style.transition = "transform " + opts.time + "s";
-        elements[i].style.transform = "";
-      }
-  
-      // proper way would be listening for `transitionend` event
-      // but fuck that, amirite
-      setTimeout(function() {
-        for (i = 0; i < j; ++i) {
-          elements[i].style.transition = "";
-          elements[i].classList.remove(opts.animatingClass, opts.scalingClass);
+  });
+
+  let finalAnimation;
+  elements.forEach((el, index) => {
+    const delta = deltas[index];
+    
+    // check if there's need for any animation
+    if (delta.x === 0 && delta.y === 0 && delta.scaleWidth === 1 && delta.scaleHeight === 1)
+       return;
+
+    // Go from the inverted position to last.
+    finalAnimation = el.animate(
+      [
+        {
+          transformOrigin: "top left",
+          transform: `translate(${delta.x}px, ${delta.y}px) scale(${delta.scaleWidth}, ${delta.scaleHeight})`
+        },
+        {
+          transformOrigin: "top left",
+          transform: "none"
         }
-        callback();
-      }, opts.time * 1000);
-    });
-  }
+      ],
+      {
+        duration: options.duration,
+        easing: options.easing
+      }
+    );
+  });
+
+  // Do any tidy up at the end
+  // of the animation.
+  if (finalAnimation && finishCallback)
+    finalAnimation.addEventListener("finish", finishCallback);
+}
--- flip/web-animation2/main.js
+++ flip/web-animation2/main.js
@@ -1,33 +1,65 @@
-import flip from './flip.js';
+import flip from "./flip.js";
 
 const container = document.querySelector(".container");
 
 document
-  .querySelectorAll("maximizable")
+  .querySelectorAll(".maximizable")
   .forEach(maxi =>
     maxi.addEventListener("click", doAnimation.bind(this, maxi))
   );
 
-let expanded = false;
+
+/**
+ * @type {HTMLElement}
+ */
+let expandedMaxi, oldParent, oldSibling;
 
 /**
  *
  * @param {HTMLElement} maxi
  */
 function doAnimation(maxi) {
+  // get all the elements that we want to animate , e.g. those that we expect to be changed after
+  // the layout change. These will be:
+  // 1. All columns
+  // 2. All children of column in which is the 'maxi' element - (e.g. it's sibling and it itself)
+  let elements = Array.from(document.querySelectorAll('.column'));
+
+  // if about to expand - remember its current parent
+  if (!expandedMaxi) {
+    oldParent = maxi.parentElement;
+    oldSibling = maxi.nextElementSibling;
+    expandedMaxi = maxi;
+  } else {
+      // TODO: 
+      if (expandedMaxi !== maxi) {
+        throw new Error('Not implemented yet');
+      }
+
+      elements.push(expandedMaxi);
+      expandedMaxi = null;
+  }
+
+  
+  elements = elements.concat(Array.from(oldParent.children));
+
   // Do animation
   flip(
-    elms, // elements to animate layout change of
+    elements, // elements to animate layout change of
 
     function triggerAction() {
       // called when we should do layout change
-      expanded = !expanded;
-      if (expanded) {
-        page.insertBefore(maxi, page.firstChild);
+
+      // if to be expanded - append the 'maxi' element to the container
+      // else return it bake to it's old position
+      if (expandedMaxi) {
+        container.insertBefore(expandedMaxi, container.firstChild);
+        expandedMaxi.classList.add("maximized");
       } else {
-        parent.insertBefore(maxi, parent.firstChild);
+        oldParent.insertBefore(maxi, oldSibling);
+        maxi.classList.remove("maximized");
       }
-      maxi.classList.toggle("maximized", expanded);
+      
     },
 
     function onDone() {
